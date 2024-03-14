f3f602258f514f0c7a4ebdf6d685b6b4e48cc2b9
rzu1303 <31926280+rzu1303@users.noreply.github.com>
2024-03-15 01:38:15 +0600

    daily push

--- config.py
+++ config.py
@@ -13,3 +13,4 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['shorttime104@gmail.com']    
+    POSTS_PER_PAGE = 15
