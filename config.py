edba8f5dbcb1b55b67138ee893134c63d338d4f8
rzu1303 <31926280+rzu1303@users.noreply.github.com>
2024-03-25 23:57:15 +0600

    daily commit

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
@@ -15,4 +17,5 @@ class Config:
     LANGUAGES = ['en', 'es']
     POSTS_PER_PAGE = 25
     MS_TRANSLATOR_KEY = os.getenv('MS_TRANSLATOR_KEY')
-    # print(MS_TRANSLATOR_KEY)
+    REGION = 'southeastasia'
+    print(MS_TRANSLATOR_KEY)
